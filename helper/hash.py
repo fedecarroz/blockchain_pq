12d671aa8239fefa272fa82fe473a2247769ae53
Federico Carrozzino <federicocarrozzino98@gmail.com>
2022-04-11 21:56:26 +0200

    upgraded hash function

--- helper/hash.py
+++ helper/hash.py
@@ -1,6 +1,7 @@
-from hashlib import sha3_256
+from hashlib import sha3_512
+
 
 def calculate_hash(message: str) -> str:
-    hash_func = sha3_256()
+    hash_func = sha3_512()
     hash_func.update(bytes(message, "utf-8"))
     return hash_func.hexdigest()
